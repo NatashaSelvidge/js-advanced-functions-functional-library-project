5c96fe7662bcba82fcc1ac191baee44d55ae6b4e
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-03-04 10:01:11 -0800

    commit tree 474b71565b53ba2d52a3b1fd2ce288fa889433c3

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const fi = (function() {
+const fi = (function () {
   return {
-    libraryMethod: function() {
-      return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
+    libraryMethod: function () {
+      return "Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0";
     },
 
-    each: function() {
-
+    each: function (collection, alert) {
+    if (Array.isArray(collection)){
+      for (let i = 0; i < collection.length; i++){
+        alert(collection[i]);    
+        }
+    }else {
+      var collect = Object.values(collection)
+      for (let i = 0; i < collect.length; i++){
+        alert(collect[i]);
+      }
+    }
+    return collection
     },
 
-    map: function() {
+    map: function () {},
 
-    },
+    reduce: function () {},
 
-    reduce: function() {
+    functions: function () {},
+  };
+})();
 
-    },
-
-    functions: function() {
-
-    },
-
-
-  }
-})()
-
-fi.libraryMethod()
+fi.libraryMethod();
